743b62e87cb1720f0a44b0e1d4f13b799ede9524
Maung945 <myothura85@gmail.com>
2023-03-24 21:51:39 -0700

    Bugs fixed

--- Newtons-Interpolation.cpp
+++ Newtons-Interpolation.cpp
@@ -1,8 +1,8 @@
 /*
-* Myo Aung CS-3010-Numerical Methods and Computing
-* Assignment4- Newton's Interpolation Polynomial Algorithm
-* 
-*/
+ * Myo Aung CS-3010-Numerical Methods and Computing
+ * Assignment4- Newton's Interpolation Polynomial Algorithm
+ *
+ */
 
 #include <iostream>
 #include <vector>
@@ -14,94 +14,114 @@
 
 using namespace std;
 
-void Coeff(vector<float>& xs, vector<float>& ys, vector<float>& cs) {
+void Coeff(vector<float> &xs, vector<float> &ys, vector<float> &cs)
+{
     int n = xs.size() - 1;
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
+    {
         cs[i] = ys[i];
     }
-    for (int j = 1; j <= n; j++) {
-        for (int i = n; i >= j; i--) {
-            cs[i] = (cs[i] - cs[i-1]) / (xs[i] - xs[i-j]);
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = n; i >= j; i--)
+        {
+            cs[i] = (cs[i] - cs[i - 1]) / (xs[i] - xs[i - j]);
         }
     }
 }
 
-float EvalNewton(vector<float>& xs, vector<float>& cs, float z) {
+float EvalNewton(vector<float> &xs, vector<float> &cs, float z)
+{
     float result = cs[xs.size() - 1];
     int n = xs.size() - 1;
-    for (int i = n-1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--)
+    {
         result = result * (z - xs[i]) + cs[i];
     }
     return result;
 }
 
-int main() {
-    // Test the implementation
-    float eval_value;
+int main()
+{
+    float eval_value, input;
     int choice, points;
     char q;
     cout << "1) For Defined Data Set." << endl;
     cout << "2) For Random Data Set." << endl;
     cin >> choice;
-    if(choice == 1) {
-        vector<float> xs {3, 1, 0, 4, 7};
-        vector<float> ys {1, 0.12, -0.3, 2, 2.5};
-        //vector<float> xs {0, 0.5, 1, 1.5};
-        //vector<float> ys {1, 1.3, 1.7, 2.1};
+    if (choice == 1)
+    {
+        vector<float> xs{3, 1, 0, 4, 7};         // Exercise 1 & 2
+        vector<float> ys{1, 0.12, -0.3, 2, 2.5}; // Exercise 1 & 2
+        // vector<float> xs {0, 0.5, 1, 1.5};
+        // vector<float> ys {1, 1.3, 1.7, 2.1};
+        auto start = chrono::high_resolution_clock::now();
         vector<float> cs(2);
         Coeff(xs, ys, cs);
-        while (true) { 
-        cout << "Enter the value to evaluate the polynomial: ";
-        cin >> eval_value;
-        
-        cout << "f(" << eval_value << ") = " << EvalNewton(xs, cs, eval_value) << endl;
-   
+        while (true)
+        {
+            char quit;
+            cout << "Enter the value to evaluate the polynomial: ";
+            cin >> eval_value;
+            cout << "f(" << eval_value << ") = " << EvalNewton(xs, cs, eval_value) << endl;
+            auto end = chrono::high_resolution_clock::now();
+            auto time_taken = chrono::duration_cast<chrono::milliseconds>(end - start);
+            cout << "Time Taken: " << time_taken.count() << "-Milliseconds" << endl;
+            cout << "Continue Yes(y)/Quit(q) : ";
+            cin >> quit;
+            if (quit == 'q')
+            {
+                break;
+            }
+            else if (quit == 'y')
+            {
+                true;
+            }
+        }
     }
-    } else if (choice == 2) {
+    else if (choice == 2)
+    {
         cout << "Enter number of points: ";
         cin >> points;
-        
-       // Create a random number generator
-        std::random_device rd;
+        std::random_device rd; // Create a random number generator
         std::mt19937 gen(rd());
-        
-        // Create a distribution to generate floats between 1 and 10
-        std::uniform_real_distribution<float> dist(1.0f, 10.0f);
-        
-        // Create two vectors to store the random numbers
-        vector<float> xcoeff(10);
-        vector<float> ycoeff(10);
-        vector<float> cs(10);
-        // Fill the vectors with random numbers
-        for (int i = 0; i < 10; i++) {
+        std::uniform_real_distribution<float> dist(1.0f, 4.0f); // Create a distribution to generate floats between 1 and 10
+        vector<float> xcoeff(points);                           // Create two vectors to store the random numbers
+        vector<float> ycoeff(points);
+        vector<float> cs(points);
+
+        for (int i = 0; i < points; i++)                        // Fill the vectors with random numbers
+        {
             xcoeff[i] = dist(gen);
             ycoeff[i] = dist(gen);
         }
-        
-        // Print the vectors
         cout << "X-Coefficient:";
-        for (auto elem : xcoeff) {
-            cout << " " << elem;
+        for (auto elem : xcoeff)
+        {
+            cout << " " << setprecision(23) << elem;
         }
         cout << endl;
-        
+
         cout << "Y-Coefficient:";
-        for (auto elem : ycoeff) {
-            cout << " " << elem;
-    }
-    cout << endl;
+        for (auto elem : ycoeff)
+        {
+            cout << " " << setprecision(23) << elem;
+        }
+        cout << endl;
         auto start = chrono::high_resolution_clock::now();
         Coeff(xcoeff, ycoeff, cs);
         cout << "Enter the value to evaluate the polynomial: ";
         cin >> eval_value;
-        
-        cout << "f(" << eval_value << ") = " << EvalNewton(xcoeff, cs, eval_value) << endl;
+
+        cout << "f(" << eval_value << ") = " << setprecision(23) << EvalNewton(xcoeff, cs, eval_value) << endl;
         auto end = chrono::high_resolution_clock::now();
-        auto time_taken = chrono::duration_cast<chrono::microseconds>(end - start);
-        cout << "Time Taken: " << time_taken.count() << "-Microseconds" << endl << endl;
-    } else {
+        auto time_taken = chrono::duration_cast<chrono::milliseconds>(end - start);
+        cout << "Time Taken: " << time_taken.count() << "-Milliseconds" << endl;
+    }
+    else
+    {
         cout << "Enter 1 or 2." << endl;
     }
-    
+
     return 0;
 }
